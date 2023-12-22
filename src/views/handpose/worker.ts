286cf53966b8bbc54d7ff3414d4c793b3d8d1d13
Arkadii Shvartsman <arkadii.shvartcman@tvbet.tv>
2023-12-22 20:00:35 +0100

    trying to optimise

--- src/views/handpose/worker.ts
+++ src/views/handpose/worker.ts
@@ -1,16 +1,5 @@
-export {};
-const ctx: Worker = self as any; // eslint-disable-line no-restricted-globals
+const context: Worker = self as any; // eslint-disable-line no-restricted-globals
 
 onmessage = async (event) => {
     console.log("event data", event.data)
-    if (event.data.message === "draw") {
-        drawPoint()
-    }
 };
-
-function drawPoint(ctx: CanvasRenderingContext2D, x: number, y: number, r: number, color: string) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.fillStyle = color;
-    ctx.fill();
-}
