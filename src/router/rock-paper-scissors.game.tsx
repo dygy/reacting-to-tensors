ad9209ad8aa31db98f35e85a70d2d9ddc032d17d
dygy <ykimurachris@gmail.com>
2023-12-31 15:59:06 +0100

    generated nav

---
+++ src/router/rock-paper-scissors.game.tsx
@@ -0,0 +1,9 @@
+import { NavigationComponent } from "@components/navigation/navigation.component";
+
+export const RockPaperScissorsGame = () => {
+  return (
+    <div>
+      <NavigationComponent />
+    </div>
+  );
+};
